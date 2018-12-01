c0ba780fe5c063a266acfe67a901a659ce7f5682
Anwar Montasir <anwarmontasir@gmail.com>
2018-11-30 17:26:06 -0800

    button is clickable

--- src/components/surprise-image.js
+++ src/components/surprise-image.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function SurpriseImage() {
-    return <img src="https://media.giphy.com/media/10dV5LFkPenFxm/giphy.gif" alt="a surprising fig" />
+export default function SurpriseImage(props) {
+    return <img src="https://media.giphy.com/media/10dV5LFkPenFxm/giphy.gif" alt="a surprising fig" onClick={props.onClick} />
 }
--- src/components/surprise.js
+++ src/components/surprise.js
@@ -6,9 +6,22 @@ import SurpriseImage from './surprise-image';
 export default class Surprise extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            surprise: false
+        }
+    }
+
+    surprise() {
+        this.setState({
+            surprise: !this.state.surprise
+        });
     }
 
     render() {
-        return <SurpriseButton />;
+        if (this.state.surprise) {
+            return <SurpriseImage onClick={e => this.surprise()} />;
+        } else {
+            return <SurpriseButton onClick={e => this.surprise()} />;
+        }
     }
 }
